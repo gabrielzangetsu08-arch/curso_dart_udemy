4b086c2edbbfe27ec96df212caff62bd0d4c7587
Gabriel <gabrielzangetsu08@gmail.com>
2026-03-12 13:17:54 -0300

    Aula Inteiros

---
+++ test/inteiros.dart
@@ -0,0 +1,17 @@
+void main() {
+  int altura = 183;
+
+  print("Altura:$altura cm");
+
+  int ano = 2026;
+
+  print("Ano: $ano");
+
+  int idade = 27;
+
+  print("Idade: $idade");
+
+  int datanascimento = ano - idade;
+
+  print("data nascimento: $datanascimento");
+}
---
+++ test/variaveis.dart
@@ -0,0 +1,35 @@
+void main() {
+  String nome = "Paulo";
+
+  nome = "Paulo mendes";
+
+  print(nome);
+
+  var idade = 17;
+
+  print(idade);
+
+  var peso = 80.5;
+
+  peso = 70;
+
+  print(peso);
+
+  var Maioridade = true;
+
+  print(Maioridade);
+
+  const nomecompleto = "Gabriel Alves Coutinho";
+
+  print(nomecompleto);
+
+  final int altura;
+
+  altura = 80;
+
+  late String nomepai;
+
+  nomepai = "jose";
+
+  print(nomepai);
+}
