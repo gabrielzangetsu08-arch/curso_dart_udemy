c234e3913cd716c25470cb119522434fbf759247
Gabriel <gabrielzangetsu08@gmail.com>
2026-03-16 12:04:23 -0300

    Aula Strings

---
+++ test/strings.dart
@@ -0,0 +1,26 @@
+void main() {
+  String nomecompleto = "Gabriel Alves Coutinho";
+
+  print("nomecompleto: $nomecompleto");
+
+  String nomegato = "Lyanna";
+
+  print("nomegato: $nomegato");
+
+  int idade = 17;
+
+  int altura = 173;
+
+  double peso = 85.6;
+
+  String informacoescompletas = '''
+nome: $nomecompleto
+idade: $idade
+altura: $altura
+peso:$peso
+
+
+ ''';
+
+  print("Informaçoes completas: $informacoescompletas");
+}
