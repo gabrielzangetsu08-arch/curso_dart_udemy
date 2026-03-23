56e62fa2256e7c093f098912093b052496039912
Gabriel <gabrielzangetsu08@gmail.com>
2026-03-23 12:43:03 -0300

    aula desafio parte 2

---
+++ test/funçoes.part.dart/desafios.part2.dart
@@ -0,0 +1,27 @@
+void main() {
+  int dia = 0;
+
+  switch (dia) {
+    case 1:
+      print("Segunda-feira");
+      break;
+    case 2:
+      print("Terça-feira");
+      break;
+    case 3:
+      print("Quarta-feira");
+      break;
+    case 4:
+      print("Quinta-feira");
+      break;
+    case 5:
+      print("Sexta-feira");
+      break;
+    case 6:
+    case 7:
+      print("Final de semana");
+      break;
+    default:
+      print("Dia inválido");
+  }
+}
